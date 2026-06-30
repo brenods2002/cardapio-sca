8b607e412f8888c799136d61beb7713cb471e39d
brenods2002 <brenods2002@gmail.com>
2026-06-30 10:07:50 -0300

    feat: add hook useCardpioModel.ts

---
+++ src/hooks/useCardapioModel.ts
@@ -0,0 +1,53 @@
+import type { Produtos } from "@/types/type";
+import { useState } from "react";
+
+interface ProdutoSelecionado extends Produtos {
+  quantidade: number;
+  observacao: string;
+}
+
+export function useCardapioModal() {
+  const [produtoModal, setProdutoModal] = useState<Produtos | null>(null);
+  const [quantidade, setQuantidade] = useState(1);
+  const [observacao, setObersvvacao] = useState("");
+
+  function handleClickSelect(produto: Produtos) {
+    setProdutoModal(produto);
+    setQuantidade(1);
+    setObersvvacao("");
+  }
+  function fecharModal() {
+    setProdutoModal(null);
+  }
+
+  function incrementar() {
+    setQuantidade((q) => q + 1);
+  }
+
+  function decrementar() {
+    setQuantidade((q) => Math.max(1, q - 1));
+  }
+
+  function confirmarSelecao(
+    onConfirmar: (produto: ProdutoSelecionado) => void,
+  ) {
+    if (!produtoModal) return;
+    onConfirmar({
+      ...produtoModal,
+      quantidade,
+      observacao,
+    });
+    fecharModal();
+  }
+  return {
+    produtoModal,
+    quantidade,
+    observacao,
+    setObersvvacao,
+    handleClickSelect,
+    fecharModal,
+    incrementar,
+    decrementar,
+    confirmarSelecao,
+  };
+}
